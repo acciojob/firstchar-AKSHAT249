1a0bb27ee1e25232e135babe5b44698479184b4c
AKSHAT249 <92660619+AKSHAT249@users.noreply.github.com>
2024-10-11 04:23:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 function firstChar(text) {
   // your code here
-	let ans = text.trim().charAt(0);
-	if(ans.length ==0){
-		return "";
-	}else{
-		return ans;
-	}
+	let trimmedText = text.trim();
+	return trimmedText.charAt(0);
 }
 
 // Do not change the code below
